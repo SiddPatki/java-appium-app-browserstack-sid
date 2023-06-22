7797a3dc5d8bf7dbd1c121db59d9a83ca6ebf2b4
Siddhartha Patki <siddharthapatki@gmail.com>
2023-06-22 22:24:13 +0530

    Update BrowserStackSample.java
    for repsuite

--- java_7/src/test/java/ios/BrowserStackSample.java
+++ java_7/src/test/java/ios/BrowserStackSample.java
@@ -3,7 +3,7 @@ package ios;
 import java.net.URL;
 import java.util.List;
 import java.net.MalformedURLException;
-
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -18,47 +18,63 @@ public class BrowserStackSample {
    	DesiredCapabilities caps = new DesiredCapabilities();
     	
     	// Set your access credentials
-    	caps.setCapability("browserstack.user", "YOUR_USERNAME");
+    	
+        caps.setCapability("browserstack.user", "YOUR_USERNAME");
     	caps.setCapability("browserstack.key", "YOUR_ACCESS_KEY");
     	
+    	
     	// Set URL of the application under test
-    	caps.setCapability("app", "bs://<app-id>");
+	caps.setCapability("app", "bs://<app-id>");
+
     	
     	// Specify device and os_version for testing
-    	caps.setCapability("device", "iPhone 12");
-    	caps.setCapability("os_version", "13");
-        
+    	caps.setCapability("device", "iPhone 14");
+    	caps.setCapability("os_version", "16");
+        //test on 1.22
     	// Set other BrowserStack capabilities
-    	caps.setCapability("project", "First Java Project");
-    	caps.setCapability("build", "browserstack-build-1");
-    	caps.setCapability("name", "first_test");
-    	
-    	
-    	// Initialise the remote Webdriver using BrowserStack remote URL
-    	// and desired capabilities defined above
+
+    	caps.setCapability("project", "stryker");
+    	caps.setCapability("build", "browserstack-stryker");
+    	caps.setCapability("name", "Repsuite");
+    	caps.setCapability("browserstack.idleTimeout", "150");
+//    	caps.setCapability("browserstack.resignApp", "false");
+//    	caps.setCapability("")
+
         IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(
-        		new URL("http://hub-cloud.browserstack.com/wd/hub"), caps);
-        
+                new URL("http://hub-cloud.browserstack.com/wd/hub"), caps);
 
-        // Test case for the BrowserStack sample iOS app. 
-        // If you have uploaded your app, update the test case here. 
-        IOSElement textButton = (IOSElement) new WebDriverWait(driver, 30).until(
-            ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Text Button")));
-        textButton.click();
-        IOSElement textInput = (IOSElement) new WebDriverWait(driver, 30).until(
-            ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Text Input")));
-        textInput.sendKeys("hello@browserstack.com");
-        Thread.sleep(5000);
-        IOSElement textOutput = (IOSElement) new WebDriverWait(driver, 30).until(
-                ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Text Output")));
-        if(textOutput != null && textOutput.getText().equals("hello@browserstack.com"))
-            assert(true);
-        else
-            assert(false);  
-    
-        // Invoke driver.quit() after the test is done to indicate that the test is completed.
-        driver.quit();
+        try {
 
+            driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+
+            IOSElement el1 = (IOSElement) driver.findElementByAccessibilityId("Continue");
+            el1.click();
+            IOSElement el2 = (IOSElement) driver.findElementByAccessibilityId("Enter your email, phone, or Skype.");
+            el2.click();
+            el2.sendKeys("rs_uat.test17@stryker.com");
+            IOSElement el3 = (IOSElement) driver.findElementByXPath("(//XCUIElementTypeButton[@name=\"Next\"])[1]");
+            el3.click();
+            IOSElement el4 = (IOSElement) driver.findElementByAccessibilityId("Enter the password for rs_uat.test17@stryker.com");
+            el4.click();
+            el4.sendKeys("Stryker1");
+            IOSElement el5 = (IOSElement) driver.findElementByAccessibilityId("Sign in");
+            el5.click();
+            IOSElement el6 = (IOSElement) driver.findElementByAccessibilityId("Yes");
+            el6.click();
+//            IOSElement el7 = (IOSElement) driver.findElementByAccessibilityId("Okay, got it!");
+//            el7.click();
+            IOSElement els1 = (IOSElement) driver.findElementByXPath("/XCUIElementTypeApplication/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]");
+            els1.click();
+            IOSElement el8 = (IOSElement) driver.findElementByXPath("(//XCUIElementTypeOther[@name=\"New Surgical Case\"])[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther");
+            el8.click();
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+            throw e;
+        }
+        finally {
+            driver.quit();
+        }
 	}
-
 }
